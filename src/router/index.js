6609534e87b67886b5013dd2fa6d120a14e623b1
王飞虎 <527064207@qq.com>
2019-08-23 10:07:03 +0800

    配置登录的路由和组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Login from '@/views/login'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  router: []
+  routes: [
+    { path: '/login', name: 'login', component: Login }
+  ]
 })
 
 export default router
