b80ef8eabaad967f9275fb83cc27c0e6e8dbd3ba
王飞虎 <527064207@qq.com>
2019-08-25 09:41:12 +0800

    欢迎界面

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,22 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Login from '@/views/login'
+import Home from '@/views/home'
+import Welcome from '@/views/welcome'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/login', name: 'login', component: Login }
+    { path: '/login', name: 'login', component: Login },
+    {
+      path: '/',
+      component: Home,
+      children: [
+        // 路由规则：自路由有名称  父路由不需要设置name属性
+        { path: '/', name: 'welcome', component: Welcome }
+      ]
+    }
   ]
 })
 
---
+++ src/views/home/index.vue
@@ -0,0 +1,134 @@
+<template>
+  <el-container class="my-container">
+    <el-aside :width="isCollapse?'64px':'200px'">
+      <!-- logo和导航 -->
+      <div class="loge" :class="{miniLogo:isCollapse}"></div>
+        <el-menu
+          default-active="1"
+          class="el-menu-vertical-demo"
+          background-color="#002033"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          :collapse="isCollapse"
+          :collapse-transition="false"
+        >
+          <el-menu-item index="1">
+            <i class="el-icon-s-home"></i>
+            <span slot="title">首页</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <i class="el-icon-document"></i>
+            <span slot="title">内容管理</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-picture"></i>
+            <span slot="title">素材管理</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-s-promotion"></i>
+            <span slot="title">发布文章</span>
+          </el-menu-item>
+          <el-menu-item index="5">
+            <i class="el-icon-chat-dot-round"></i>
+            <span slot="title">评论管理</span>
+          </el-menu-item>
+          <el-menu-item index="6">
+            <i class="el-icon-present"></i>
+            <span slot="title">粉丝管理</span>
+          </el-menu-item>
+          <el-menu-item index="7">
+            <i class="el-icon-setting"></i>
+            <span slot="title">个人设置</span>
+          </el-menu-item>
+        </el-menu>
+    </el-aside>
+    <el-container>
+      <!-- 右侧顶部 -->
+      <el-header>
+        <span @click="toggleAside()" class="icon el-icon-s-fold"></span>
+        <span class="text">江苏传智播客科技教育有限公司</span>
+        <el-dropdown class="my-dropdown">
+          <span class="el-dropdown-link">
+            <img class="avatar" src="../../assets/images/avatar.jpg" alt />
+            <span class="name">用户名称</span>
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item icon="el-icon-setting">个人设置</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-unlock">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
+      <!-- 右侧内容 -->
+      <el-main>
+        <!-- 二级路由 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isCollapse: false
+    }
+  },
+  methods: {
+    toggleAside () {
+      this.isCollapse = !this.isCollapse
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.my-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  .el-aside {
+    background: #002033;
+    .loge {
+      width: 100%;
+      height: 60px;
+      background: #002244 url(../../assets/images/logo_admin.png) no-repeat
+        center / 140px auto;
+    }
+    .miniLogo{
+      background-image: url(../../assets/images/logo_admin_01.png);
+      background-size: 36px auto;
+    }
+    .el-menu{
+          border-right: none;
+        }
+  }
+  .el-header {
+    border-bottom: 1px solid #002033;
+    line-height: 60px;
+    .icon {
+      font-size: 24px;
+      vertical-align: middle;
+    }
+    .text {
+      vertical-align: middle;
+    }
+    .my-dropdown {
+      float: right;
+      .avatar {
+        width: 30px;
+        height: 30px;
+        vertical-align: middle;
+      }
+      .name {
+        color: #333;
+        font-weight: bold;
+        vertical-align: middle;
+      }
+    }
+  }
+}
+</style>
