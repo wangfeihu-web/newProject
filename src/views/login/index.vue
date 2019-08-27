f60dbbb1ec8228d3e07988b21d4c78435a452e8d
王飞虎 <527064207@qq.com>
2019-08-27 14:45:14 +0800

    修改登录代码async、await

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,24 +1,28 @@
 <template>
-    <div class="container" ref="box">
-        <el-card class="my-card">
-            <img src="../../assets/images/logo_index.png" alt="">
-            <el-form ref="loginForm" :model="loginForm" :rules="loginRules" status-icon>
-                <el-form-item prop="mobile">
-                    <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
-                </el-form-item>
-                <el-form-item prop="code">
-                    <el-input v-model="loginForm.code"  placeholder="请输入密码" style="width:237px; margin-right:10px"></el-input>
-                    <el-button>发送验证码</el-button>
-                </el-form-item>
-                <el-form-item>
-                    <el-checkbox :value='true'>我已阅读并同意用户协议和隐私条款</el-checkbox>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type='primary' @click="login()" style="width:100%;">登 录</el-button>
-                </el-form-item>
-            </el-form>
-        </el-card>
-    </div>
+  <div class="container" ref="box">
+    <el-card class="my-card">
+      <img src="../../assets/images/logo_index.png" alt />
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" status-icon>
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入密码"
+            style="width:237px; margin-right:10px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="login()" style="width:100%;">登 录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -59,18 +63,28 @@ export default {
   },
   methods: {
     login () {
-      this.$refs.loginForm.validate((valid) => {
+      this.$refs.loginForm.validate(async valid => {
         if (valid) {
-          this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm)
-            .then(res => {
-              // 用户信息  res.data 是响应的主体 在.data就是用户信息
-              // 将操作用户信息 写在store中
-              store.setUser(res.data.data)
-              this.$router.push('/')
-            })
-            .catch(() => {
-              this.$message.error('手机号或验证码错误')
-            })
+          // this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm)
+          //   .then(res => {
+          //     // 用户信息  res.data 是响应的主体 在.data就是用户信息
+          //     // 将操作用户信息 写在store中
+          //     store.setUser(res.data.data)
+          //     this.$router.push('/')
+          //   })
+          //   .catch(() => {
+          //     this.$message.error('手机号或验证码错误')
+          //   })
+
+          try {
+            const {
+              data: { data }
+            } = await this.$http.post('authorizations', this.loginForm)
+            store.setUser(data)
+            this.$router.push('/')
+          } catch (exception) {
+            this.$message.error('手机号或验证码错误')
+          }
         }
       })
     }
@@ -79,25 +93,25 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.container{
-    background:url(../../assets/images/login_bg.jpg) no-repeat center/cover;
-    width: 100%;
-    height: 100%;
+.container {
+  background: url(../../assets/images/login_bg.jpg) no-repeat center/cover;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  .my-card {
+    width: 400px;
+    height: 350px;
     position: absolute;
-    left: 0;
-    top: 0;
-    .my-card{
-        width: 400px;
-        height: 350px;
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%,-50%);
-        img {
-            width: 200px;
-            display: block;
-            margin: 0 auto 20px;
-        }
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      width: 200px;
+      display: block;
+      margin: 0 auto 20px;
     }
+  }
 }
 </style>
